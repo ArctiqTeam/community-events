c3e627d68bdbe0cc7b8b6e8725cf2ee3366d5559
Aren <7918248+funixz@users.noreply.github.com>
2019-01-25 12:52:22 -0500

    Update tf_make-dns.tf

--- ansible-tower-azure-webinar/tower/tf_make-dns.tf
+++ ansible-tower-azure-webinar/tower/tf_make-dns.tf
@@ -1,22 +1,22 @@
 provider "vault" {
-  address = "https://vault.arctiq.ca"
+  address = "https://<your vault address here>"
 }
 
 data "vault_generic_secret" "gcp_auth" {
-  path = "gcp/key/funixz_dns"
+  path = "<path to your secret key>"
 }
 
 provider "google" {
   credentials = "${base64decode(data.vault_generic_secret.gcp_auth.data.private_key_data)}"
 }
 
-resource "google_dns_record_set" "towaz" {
-  name         = "towaz.funixz.team.arctiq.ca."
+resource "google_dns_record_set" "<name the record>" {
+  name         = "<your DNS name (FQDN) for the node>."
   type         = "A"
   ttl          = 300
-  managed_zone = "funixz-zone"
-  rrdatas      = ["13.66.173.12"]
-  project      = "funixz-zone"
+  managed_zone = "<a dns zone in gcp>"
+  rrdatas      = ["IP for the tower node"]
+  project      = "<gcp project providing the zone>"
 }
 
 
